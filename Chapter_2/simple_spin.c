3cee57237e1c10c26f8e34274c0c357480c73666
yusen <liuys1997@gmail.com>
2021-01-13 00:19:37 -0600

    simple_spin on page 5 added

---
+++ Chapter_2/simple_spin.c
@@ -0,0 +1,29 @@
+/*
+  page 5
+  $ gcc -o cpu simple_spin.c -Wall -Werror
+  $ ./cpu A & ./cpu B & ./cpu C & ./cpu D &
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+int
+main(int argc, char* argv[])
+{
+  if(argc != 2){
+    fprintf(stderr, "usage: cpu <string>\n");
+    exit(1);
+  }
+  
+  char* str = argv[1];
+
+  while(1){
+    sleep(1); // in OSTEP, it uses Spin(1)
+    printf("%s\n", str);
+  }
+  
+  return 0;
+}
